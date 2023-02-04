b38dfbdcf1fecfe17996cdf9a5cb74ded90965bc
matsuurayuki1219 <yuki.matsuura@progrit.co.jp>
2023-02-04 17:02:00 +0900

    chore: デバッグログ出力

--- app/src/main/java/jp/matsuura/householdaccountapp/App.kt
+++ app/src/main/java/jp/matsuura/householdaccountapp/App.kt
@@ -2,6 +2,30 @@ package jp.matsuura.householdaccountapp
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import timber.log.Timber
 
 @HiltAndroidApp
-class App: Application()
+class App: Application() {
+
+    private val applicationScope = CoroutineScope(Dispatchers.Default)
+    
+    override fun onCreate() {
+        super.onCreate()
+        delayedInit()
+    }
+
+    private fun delayedInit() {
+        applicationScope.launch {
+            initTimber()
+        }
+    }
+
+    private fun initTimber() {
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+}
